d0c112bde1961335d63db6b75726553571e6e3f5
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-08-04 20:54:49 -0500

    dsfsdf

---
+++ plugins/vsn.js
@@ -0,0 +1,23 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.bsfem);
+};
+handler.command = /^(vsn|nortevs)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.bsfem = ` 
+VERSUS CONTRA PEPES ᡣ𐭩
+⏰(    ) 🇵🇪 - (    ) 🇦🇷🇨🇱 
+🖇️𝗥𝗶𝘃𝗮𝗹: 
+✨𝗜𝗴: 
+•𝐄𝐬𝐜𝐮𝐚𝐝𝐫𝐚•
+👾
+👾
+👾
+👾
+•𝐒𝐮𝐩𝐥𝐞𝐧𝐭𝐞𝐬•
+👸🏻
+🫅🏻
+`;
